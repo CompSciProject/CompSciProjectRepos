e42ac88f4c2571d702022f6c3471aae1cf5a7707
joelnmdyer <joel.dyer@maths.ox.ac.uk>
2018-10-16 17:29:39 +0100

    Capitalised function names

--- PredCorr/code/FullPredCorr.m
+++ PredCorr/code/FullPredCorr.m
@@ -14,7 +14,7 @@ function sol = completePredCorr(y_0, f, t_fin, N)
     sol = [y_0];
     dt = t_fin/N;
     for i = 1:N
-        sol(i+1) = stepPredCorr(sol(i), dt, i*dt, f);
+        sol(i+1) = StepPredCorr(sol(i), dt, i*dt, f);
     end
 
 end
--- PredCorr/code/RunPredCorr.m
+++ PredCorr/code/RunPredCorr.m
@@ -4,8 +4,8 @@
 % Predictor-Corrector uses it
 addpath('../../ExEul/code/')
 
-func = @(t, y) sin(y)*sin(t);
-y0 = 0;
+func = @(t, y) -y;
+y0 = 0.1;
 t_fin = 10;
-N = 10;
-fullPredCorr(y0, func, t_fin, N)
+N = 100;
+FullPredCorr(y0, func, t_fin, N)
