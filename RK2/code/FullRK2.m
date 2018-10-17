0e9bbaa64884a7f792f0be395d9e4c0ed50d7ff8
huining yang <yang@maths.ox.ac.uk>
2018-10-17 17:01:49 +0100

    added RK2files

---
+++ RK2/code/FullRK2.m
@@ -0,0 +1,23 @@
+function sol = FullRK2(t_fin,y_0,f, N)
+% FULLRK2  solver of IVPs by applying 2nd order Runge-Kutta method 
+%    Input 
+%            - t_fin      final time/value of independent variable
+%            - y_0        initial value
+%            - f          functional form of derivative
+%            - N          number of steps to take
+%    Output
+%            - sol        solution array 
+
+    h=t_fin/N;
+    times=0:h:t_fin; %define vector of time
+    solutions=nan(1,N+1); %define blank vector in which to place solutions
+    solutions(1,1) = y_0;
+    
+    for i=1:N
+        solutions(i+1) = StepRK2(solutions(i), (i-1)*h,h,f);
+    end
+ 
+    sol = [solutions'];
+    plot(times,solutions,'bo-')
+    shg
+end
