ec2a17bf4156127a875f833db86462fe0cad2824
huining yang <yang@maths.ox.ac.uk>
2018-10-17 17:18:40 +0100

    I added t_0 as input

--- RK2/code/FullRK2.m
+++ RK2/code/FullRK2.m
@@ -1,6 +1,7 @@
-function sol = FullRK2(t_fin,y_0,f, N)
+function sol = FullRK2(t_0,t_fin,y_0,f,N)
 % FULLRK2  solver of IVPs by applying 2nd order Runge-Kutta method 
 %    Input 
+%            - t_0        initial time
 %            - t_fin      final time/value of independent variable
 %            - y_0        initial value
 %            - f          functional form of derivative
@@ -8,13 +9,13 @@ function sol = FullRK2(t_fin,y_0,f, N)
 %    Output
 %            - sol        solution array 
 
-    h=t_fin/N;
-    times=0:h:t_fin; %define vector of time
+    h=(t_fin-t_0)/N;
+    times=t_0:h:t_fin; %define vector of time
     solutions=nan(1,N+1); %define blank vector in which to place solutions
     solutions(1,1) = y_0;
     
     for i=1:N
-        solutions(i+1) = StepRK2(solutions(i), (i-1)*h,h,f);
+        solutions(i+1) = StepRK2(solutions(i), t_0+(i-1)*h,h,f);
     end
  
     sol = [solutions'];
