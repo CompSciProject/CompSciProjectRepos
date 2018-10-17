f2171cf939ecfa17d761588e0d22c1bc73122bbc
Oliver Bond <bond@maths.ox.ac.uk>
2018-10-17 16:51:23 +0100

    Implicit Euler files now output vectors for time as well as Y

--- ImEul/code/FullImEul.m
+++ ImEul/code/FullImEul.m
@@ -8,18 +8,20 @@
 %      - N:         number of steps to take
 %
 % Output:
+%      - time:      vector of values of t
 %      - sol:       solution array
 
-function sol = FullImEul(t_0,t_fin,y_0, f,N)
+function [time,sol] = FullImEul(t_0,t_fin,y_0, f,N)
 
+    time = linspace(t_0,t_fin,N+1).';  % Creates set of t-values
     sol = nan(1,N+1).';  % Defines space of values for solution
-    sol(1) = [y_0];     % Sets first value of solution vector to be initial condition
+    sol(1) = [y_0];      % Sets first value of solution vector to be initial condition
      
-    dt = (t_fin - t_0)/N;   % Calculates one time step
+    dt = (t_fin-t_0)/N;  % Calculates one time step
     for i = 1:N
-        sol(i+1) = StepImEul(sol(i),dt,t_0 + i*dt,f);
+        sol(i+1) = StepImEul(sol(i),dt,time(i+1),f);
         
     % Runs the Implcit Euler scheme in full by calling StepImEul.m
     % and repeating this for the number of steps required from the inputs
-    end
+    end 
 end
--- ImEul/code/RunImEul.m
+++ ImEul/code/RunImEul.m
@@ -4,7 +4,7 @@ func = @(t,y) -y;
 y0 = 0.1;
 t_fin = 10;
 N = 100;
-FullImEul(0, t_fin, y0, func, N)
+[T,Y] = FullImEul(0, t_fin, y0, func, N);
 
 % Solves the IVP dy/dt = f(t,y) on [0,10] with f(t,y) = -y, given that 
 % y(0) = 0.1 
