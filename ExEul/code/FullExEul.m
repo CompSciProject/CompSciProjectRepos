6f29d6f1989c09fa1fc0561ba2fce2f70ecdcd36
Arkady Wey <arkady.wey@maths.ox.ac.uk>
2018-10-17 17:08:53 +0100

    changing notation for transpose..

--- ExEul/code/FullExEul.m
+++ ExEul/code/FullExEul.m
@@ -14,8 +14,8 @@ function output=FullExEul(t_0,t_fin,y_0,f,N)
 
     h=(t_fin-t_0)/N;
     times=0:h:t_fin; %define vector of time
-    sol=nan(1,N+1).'; %define blank vector in which to place solutions
-    sol(1,1) = y_0; %define starting position
+    sol=nan(N+1,1); %define blank vector in which to place solutions
+    sol(1,1) = y_0; %store initial position
     for ii=1:N
                                     
         func_value = f(t_0+ii*h, sol(ii)); %calculates the gradient function value to input to step
